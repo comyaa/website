ce404814a98fff4f6ba672b26b02442cfaa94cb4
cvrcsit <36961961+csitprojects@users.noreply.github.com>
2021-09-11 09:15:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -141,18 +141,5 @@ function getAbsenties() {
 
 
 }
-function copy() {
-    /* Get the text field */
-    var copyText = document.getElementById("rrr");
-  
-    /* Select the text field */
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); /* For mobile devices */
-  
-    /* Copy the text inside the text field */
-    navigator.clipboard.writeText(copyText.value);
-    
-    /* Alert the copied text */
-    alert("Copied the text " );
-  }
+
 
